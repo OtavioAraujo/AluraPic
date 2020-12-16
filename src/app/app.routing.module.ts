459da3a627a7a7aa59513350635edcc6db9672a1
Otávio Araújo Jardim <otavioajardim@gmail.com>
2020-12-16 20:54:56 -0300

    Adição da rota de "not found"

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,14 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
     { path: 'user/flavio', component: PhotoListComponent },
-    { path: 'p/add', component: PhotoFormComponent }
+    { path: 'p/add', component: PhotoFormComponent },
+    { path: '**', component: NotFoundComponent } // ** = Qualquer rota inválida
 ];
 
 @NgModule({
-    imports: [ RouterModule.forRoot(routes) ],
-    exports: [ RouterModule ]
+    imports: [ 
+        RouterModule.forRoot(routes) 
+    ],
+    exports: [ 
+        RouterModule 
+    ]
 })
 export class AppRoutingModule {}
