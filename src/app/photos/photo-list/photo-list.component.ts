115b382c7bb7d43a4b187f2c5f7740f4278a8959
Otávio Araújo Jardim <otavioajardim@gmail.com>
2020-12-25 15:57:58 -0300

    Criação da paginação das imagens

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { debounceTime }  from 'rxjs/operators';
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 
 @Component({
@@ -15,10 +16,17 @@ export class PhotoListComponent implements OnInit {
   photos:Photo[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = ''
 
-  constructor(private activatedRoute: ActivatedRoute) {} // representa a rota ativa no momento atual
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) {} // representa a rota ativa no momento atual
   
   ngOnInit(): void {   
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
     this.debounce
       .pipe(debounceTime(300))
@@ -27,5 +35,21 @@ export class PhotoListComponent implements OnInit {
 
   ngOnDestroy() {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+
+        // Não funcionaría usar o push, pois o mecânismo de change detect do angular NÃO iria conseguir detectar a mudança
+        // O mecânismo de change detect do angular apenas detecta a mudança em uma in-bound property, se
+        // um novo valor ou referência for atribuida a ela.
+
+        // this.photos.push(...photos);
+
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) this.hasMore = false;
+      })
   }
 }
