06f4ff25f35bcab9d3cd7064b75de638e50d7d90
Otávio Araújo Jardim <otavioajardim@gmail.com>
2020-12-25 22:51:01 -0300

    Criação de card e de componente do filtro

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime }  from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
@@ -15,7 +14,6 @@ export class PhotoListComponent implements OnInit {
 
   photos:Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = ''
@@ -28,20 +26,15 @@ export class PhotoListComponent implements OnInit {
   ngOnInit(): void {   
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe( filter => this.filter = filter)
-  }
-
-  ngOnDestroy() {
-    this.debounce.unsubscribe();
   }
 
   load() {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(photos => {
-
+        
+        this.filter = '';
+        
         // Não funcionaría usar o push, pois o mecânismo de change detect do angular NÃO iria conseguir detectar a mudança
         // O mecânismo de change detect do angular apenas detecta a mudança em uma in-bound property, se
         // um novo valor ou referência for atribuida a ela.
