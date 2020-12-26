ae36dffdedaf06b0bbd8d21d91b0608ccef39d92
Otávio Araújo Jardim <otavioajardim@gmail.com>
2020-12-26 15:16:35 -0300

    Criação de formulário de login na pagina home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 import { SigninComponent } from './signin/signin.component';
 
@@ -8,7 +10,9 @@ import { SigninComponent } from './signin/signin.component';
         SigninComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule
     ]
 })
 export class HomeModule {}
