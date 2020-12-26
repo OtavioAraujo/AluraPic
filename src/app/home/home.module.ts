2eb38ae6fe2c584b525cd88c03ff8b90354d56ad
Otávio Araújo Jardim <otavioajardim@gmail.com>
2020-12-26 11:44:44 -0300

    Criação do componente signin com escopo de pagina para realiar login na aplicação

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { SigninComponent } from './signin/signin.component';
+
+@NgModule({
+    declarations: [
+        SigninComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+export class HomeModule {}
