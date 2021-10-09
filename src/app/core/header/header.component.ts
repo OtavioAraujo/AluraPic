b6104675db4ace75dc74e780bac3b2cea5a9e2bb
Otávio Araújo <otavioajardim@gmail.com>
2021-10-09 18:25:55 -0300

    Adiciona o template no HeaderComponent

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'ap-header'
+    selector: 'ap-header',
+    templateUrl: './header.component.html'
 })
 export class HeaderComponent {
 
