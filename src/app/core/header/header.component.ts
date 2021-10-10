cbae44d48228a84eafa7bc4dd74b9661432f6c55
Otávio Araújo <otavioajardim@gmail.com>
2021-10-10 16:05:25 -0300

    Adiciona uso do UserService no header para obter nome do user

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+import { UserService } from '../user/user.service';
 
 @Component({
     selector: 'ap-header',
@@ -6,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+    user$: Observable<User>
+    user: User
+
+    constructor(private userService: UserService) {
+        this.user$ = this.userService.getUser()
+        this.user$.subscribe(user => this.user = user)
+
+    }
 }
