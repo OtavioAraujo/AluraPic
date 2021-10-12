0d4ba1dad2b0ec4bdadc242e57da1f3117e1d8e1
Otávio Araújo <otavioajardim@gmail.com>
2021-10-12 16:52:24 -0300

    Adiciona validação no form de cadastro de usuário

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: 'signup.component.html'
@@ -12,10 +12,26 @@ export class SignupComponent implements OnInit {
 
     ngOnInit(): void {
         this.signupForm = this.formBuilder.group({
-            email: [''],
-            fullName: [''],
-            userName: [''],
-            password: ['']
+            email: ['', [
+                Validators.required,
+                Validators.email
+            ]],
+            fullName: ['', [
+                Validators.required,
+                Validators.minLength(2),
+                Validators.maxLength(40)
+            ]],
+            userName: ['', [
+                Validators.required,
+                Validators.pattern(/^[a-z0-9_\-]+$/),
+                Validators.minLength(2),
+                Validators.maxLength(30)
+            ]],
+            password: ['', [
+                Validators.required,                
+                Validators.minLength(8),
+                Validators.maxLength(14)
+            ]]
         })
     }
 
