2cf3bff491ceb4d313885137a4008092c7348daf
Otávio Araújo <otavioajardim@gmail.com>
2021-10-12 11:12:49 -0300

    Cria componente pagina de sign up

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
     declarations: [
-        SigninComponent
+        SigninComponent,
+        SignupComponent
     ],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
         RouterModule
